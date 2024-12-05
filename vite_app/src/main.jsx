eb5a9b0f3271b51e813c291f90127c7889e7fc4e
sachaniyanarvin <sachaniyanarvin21@gmail.com>
2024-12-05 16:41:12 +0530

    first commit

--- vite_app/src/main.jsx
+++ vite_app/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Spotify from './spotify.jsx'
+// import Spotify from './spotify.jsx'
+import Sidebar from './components/Sidebar.jsx'
+import PlayControls from './components/PlayControls.jsx'
+import MainMenu from './components/MainMenu.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Spotify />
+    {/* <Spotify /> */}
+    <Sidebar />
+    <PlayControls />
+    <MainMenu />
   </StrictMode>,
 )
